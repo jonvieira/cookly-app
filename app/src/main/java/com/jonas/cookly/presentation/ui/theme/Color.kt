6e4a1a02cff0ff7ce79e8694be332473572d0c87
Jonas Vieira <jonasvieira.ti@gmail.com>
2025-06-27 18:42:45 -0300

    refactor: move UI related files to presentation package

--- app/src/main/java/com/jonas/cookly/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/jonas/cookly/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.jonas.cookly.ui.theme
+package com.jonas.cookly.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/jonas/cookly/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/jonas/cookly/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.jonas.cookly.ui.theme
+package com.jonas.cookly.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
