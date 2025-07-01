43048abf6c3639eab90682a93188c06713744613
Jonas Vieira <jonasvieira.ti@gmail.com>
2025-06-30 22:15:55 -0300

    feat: create use case to validate register fields

---
+++ app/src/main/java/com/jonas/cookly/presentation/features/register/domain/model/RegisterInputValidationType.kt
@@ -0,0 +1,13 @@
+package com.jonas.cookly.presentation.features.register.domain.model
+
+enum class RegisterInputValidationType {
+    EmptyField,
+    NoEmail,
+    PasswordsDoNotMatch,
+    PasswordUpperCaseMissing,
+    PasswordNumberMissing,
+    PasswordSpecialCharMissing,
+    PasswordTooShort,
+    PhoneNumberInvalid,
+    Valid
+}
