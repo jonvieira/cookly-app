75e84a9f96a1e8c9f0c2f2b0e810f6b703196dff
Jonas Vieira <jonasvieira.ti@gmail.com>
2025-08-12 09:25:38 -0300

    feat: create data store structure

---
+++ app/src/main/java/com/jonas/cookly/core/data/local/DataStoreLocalSource.kt
@@ -0,0 +1,11 @@
+package com.jonas.cookly.core.data.local
+
+import com.jonas.cookly.core.domain.UserData
+import kotlinx.coroutines.flow.Flow
+
+
+interface DataStoreLocalSource {
+    fun getData(): Flow<UserData>
+    suspend fun saveData(userData: UserData)
+    suspend fun clearData()
+}
